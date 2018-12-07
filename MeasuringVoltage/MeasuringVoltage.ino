3ce25b2d30d0a1cd807d235a118b595baae93be0
Christopher Nethercott <ccnethercott@gmail.com>
2018-12-07 18:35:14 +0000

    Comments and fixed debug

--- MeasuringVoltage/MeasuringVoltage.ino
+++ MeasuringVoltage/MeasuringVoltage.ino
@@ -1,6 +1,6 @@
 // Measuring Voltage.
 const int LEDArray[] = {2, 3, 4, 5, 6, 7, 8, 9};
-const bool DEBUG = true;
+const bool debug = true;
 
 void setup() {
   pinMode(A0, INPUT); // pin to measure the voltage
@@ -13,7 +13,7 @@ void setup() {
 }
 
 void loop() {
-  int tmp = map(analogRead(A0), 0, 1023, 0, sizeof(LEDArray) / 2);
+  int tmp = map(analogRead(A0), 0, 1023, 0, sizeof(LEDArray) / 2); // convert analogRead from 0-1023 to the number of LEDs we have.
   if (debug) {
     Serial.print("Analog Value: ");
     Serial.print(analogRead(A0));
