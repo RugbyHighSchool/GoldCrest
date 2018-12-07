e18debc209e86d6bb81dcfc819945ab8d10da685
Christopher Nethercott <ccnethercott@gmail.com>
2018-12-07 17:31:39 +0000

    Simple up and down using some LEDs

--- MeasuringVoltage/MeasuringVoltage.ino
+++ MeasuringVoltage/MeasuringVoltage.ino
@@ -1,10 +1,10 @@
 // Measuring Voltage.
-const int LEDArray[] = {2,3,4,5,6}
+const int LEDArray[] = {2,3,4,5,6,7,8,9};
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(A0, INPUT); // pin to measure the voltage
-  for(int i = 0; i < LEDArray.length; i++) {
+  for(int i = 0; i < sizeof(LEDArray); i++) {
     pinMode(LEDArray[i], OUTPUT);
   }
   
@@ -12,5 +12,16 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  for(int j=0; j < sizeof(LEDArray); j++) {
+    digitalWrite(LEDArray[j], HIGH);
+    delay(500);
+  }
 
+  delay(2000);
+
+  for(int k=0; k < sizeof(LEDArray); k++) {
+    digitalWrite(LEDArray[k], LOW);
+    delay(500);
+  }
 }
+ 
