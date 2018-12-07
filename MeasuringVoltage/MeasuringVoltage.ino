5313e7dfe34a09515102912b0a3629efe045a297
Christopher Nethercott <ccnethercott@gmail.com>
2018-12-07 18:33:12 +0000

    Formatting

--- MeasuringVoltage/MeasuringVoltage.ino
+++ MeasuringVoltage/MeasuringVoltage.ino
@@ -1,29 +1,29 @@
 // Measuring Voltage.
-const int LEDArray[] = {2,3,4,5,6,7,8,9};
+const int LEDArray[] = {2, 3, 4, 5, 6, 7, 8, 9};
 const bool DEBUG = true;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(A0, INPUT); // pin to measure the voltage
-  for(int i = 0; i < sizeof(LEDArray); i++) {
+  for (int i = 0; i < sizeof(LEDArray); i++) {
     pinMode(LEDArray[i], OUTPUT);
   }
-  if (debug) { 
-  Serial.begin(9600);
+  if (debug) {
+    Serial.begin(9600);
   }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-//  Serial.println(analogRead(A0));
-  int tmp = map(analogRead(A0), 0, 1023, 0, sizeof(LEDArray)/2);
+  //  Serial.println(analogRead(A0));
+  int tmp = map(analogRead(A0), 0, 1023, 0, sizeof(LEDArray) / 2);
   if (debug) {
-  Serial.print("Analog Value: ");
-  Serial.print(analogRead(A0));
-  Serial.print(" Temp Value: ");
-  Serial.println(tmp);
+    Serial.print("Analog Value: ");
+    Serial.print(analogRead(A0));
+    Serial.print(" Temp Value: ");
+    Serial.println(tmp);
   }
-  
+
   for (int j = 0; j < tmp; j++) {
     digitalWrite(LEDArray[j], HIGH);
   }
