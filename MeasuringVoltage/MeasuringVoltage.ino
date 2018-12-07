efb7e0d326a3645b988d168ff77d0825fabb382e
Christopher Nethercott <ccnethercott@gmail.com>
2018-12-07 15:33:17 +0000

    Using a more traditionally for-loop

--- MeasuringVoltage/MeasuringVoltage.ino
+++ MeasuringVoltage/MeasuringVoltage.ino
@@ -4,9 +4,10 @@ const int LEDArray[] = {2,3,4,5,6}
 void setup() {
   // put your setup code here, to run once:
   pinMode(A0, INPUT); // pin to measure the voltage
-  for(int pin : LEDArray) {
-    pinMode(pin, OUTPUT);
+  for(int i = 0; i < LEDArray.length; i++) {
+    pinMode(LEDArray[i], OUTPUT);
   }
+  
 }
 
 void loop() {
