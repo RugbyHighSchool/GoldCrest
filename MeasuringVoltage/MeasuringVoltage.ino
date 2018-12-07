86640cff32a81f9a0048f4c492ff3d493ba2899b
Christopher Nethercott <ccnethercott@gmail.com>
2018-12-07 18:33:05 +0000

    Very simple debug function

--- MeasuringVoltage/MeasuringVoltage.ino
+++ MeasuringVoltage/MeasuringVoltage.ino
@@ -1,5 +1,6 @@
 // Measuring Voltage.
 const int LEDArray[] = {2,3,4,5,6,7,8,9};
+const bool DEBUG = true;
 
 void setup() {
   // put your setup code here, to run once:
@@ -7,17 +8,21 @@ void setup() {
   for(int i = 0; i < sizeof(LEDArray); i++) {
     pinMode(LEDArray[i], OUTPUT);
   }
+  if (debug) { 
   Serial.begin(9600);
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 //  Serial.println(analogRead(A0));
   int tmp = map(analogRead(A0), 0, 1023, 0, sizeof(LEDArray)/2);
+  if (debug) {
   Serial.print("Analog Value: ");
   Serial.print(analogRead(A0));
   Serial.print(" Temp Value: ");
   Serial.println(tmp);
+  }
   
   for (int j = 0; j < tmp; j++) {
     digitalWrite(LEDArray[j], HIGH);
