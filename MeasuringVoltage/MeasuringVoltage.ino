d92ce7ed82f4af7d692c28dfbeaea16a4140766a
Christopher Nethercott <ccnethercott@gmail.com>
2018-12-08 15:57:07 +0000

    Formatting

--- MeasuringVoltage/MeasuringVoltage.ino
+++ MeasuringVoltage/MeasuringVoltage.ino
@@ -2,7 +2,7 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-const int LEDArray[] = {2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13};
+const int LEDArray[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 const bool debug = true;
 
 int currentValue = 0;
@@ -26,16 +26,16 @@ void setup() {
 
 void loop() {
   currentValue = map(analogRead(A0), 0, 1000, 0, sizeof(LEDArray) / 2);
- 
+
   if (currentValue != previousValue) {
     if (currentValue < previousValue) {
       for (int i = 0; i < sizeof(LEDArray) / 2; i++) {
         digitalWrite(LEDArray[i], LOW);
       }
     }
-      lcd.setCursor(0,1);
+    lcd.setCursor(0, 1);
     lcd.print("Voltage: ");
-    lcd.print(map(analogRead(A0), 0, 1023, 0, 500)/100.0);
+    lcd.print(map(analogRead(A0), 0, 1023, 0, 500) / 100.0);
     lcd.print("V");
     for (int j = 0; j < currentValue; j++) {
       digitalWrite(LEDArray[j], HIGH);
