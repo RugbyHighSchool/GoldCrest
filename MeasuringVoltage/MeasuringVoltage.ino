e91c701001e757bfe3b0e7dbe86595be62d02fb3
Christopher Nethercott <ccnethercott@gmail.com>
2018-12-08 13:09:56 +0000

    Wanting to use a LCD as well

--- MeasuringVoltage/MeasuringVoltage.ino
+++ MeasuringVoltage/MeasuringVoltage.ino
@@ -1,6 +1,12 @@
 // Measuring Voltage.
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
 const int LEDArray[] = {2, 3, 4, 5, 6, 7, 8, 9};
 const bool debug = true;
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 
 void setup() {
   pinMode(A0, INPUT); // pin to measure the voltage
@@ -10,6 +16,9 @@ void setup() {
   if (debug) {
     Serial.begin(9600);
   }
+
+  lcd.begin();
+  lcd.print("RHS Gold Crest");
 }
 
 void loop() {
@@ -20,6 +29,9 @@ void loop() {
     Serial.print(" Temp Value: ");
     Serial.println(tmp);
   }
+  lcd.setCursor(0,1);
+  lcd.print("Voltage: ");
+  lcd.print(analogRead(A0));
 
   for (int j = 0; j < tmp; j++) {
     digitalWrite(LEDArray[j], HIGH);
@@ -28,4 +40,5 @@ void loop() {
   for (int i = 0; i < sizeof(LEDArray)/2; i++) {
     digitalWrite(LEDArray[i], LOW);
   }
+  delay(100);
 }
