28acebd15699659c777e8274dc544ef2ce9d2dbd
Christopher Nethercott <ccnethercott@gmail.com>
2018-12-08 15:56:59 +0000

    Added some more LEDs!

--- MeasuringVoltage/MeasuringVoltage.ino
+++ MeasuringVoltage/MeasuringVoltage.ino
@@ -2,7 +2,7 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-const int LEDArray[] = {2, 3, 4, 5, 6, 7, 8, 9};
+const int LEDArray[] = {2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13};
 const bool debug = true;
 
 int currentValue = 0;
@@ -21,12 +21,11 @@ void setup() {
   }
 
   lcd.begin();
-  lcd.print("RHS Gold Crest");
-  lcd.setCursor(0,1);
+  lcd.print("Electroute RHS");
 }
 
 void loop() {
-  currentValue = map(analogRead(A0), 0, 1020, 0, sizeof(LEDArray) / 2);
+  currentValue = map(analogRead(A0), 0, 1000, 0, sizeof(LEDArray) / 2);
  
   if (currentValue != previousValue) {
     if (currentValue < previousValue) {
@@ -34,9 +33,10 @@ void loop() {
         digitalWrite(LEDArray[i], LOW);
       }
     }
+      lcd.setCursor(0,1);
     lcd.print("Voltage: ");
-    lcd.print(currentValue);
-    lcd.print("mV");
+    lcd.print(map(analogRead(A0), 0, 1023, 0, 500)/100.0);
+    lcd.print("V");
     for (int j = 0; j < currentValue; j++) {
       digitalWrite(LEDArray[j], HIGH);
     }
@@ -44,7 +44,7 @@ void loop() {
   }
 
   previousValue = currentValue;
-
+  delay(20); // stability
   if (debug) {
     Serial.print(currentValue);
     Serial.print(" : ");
