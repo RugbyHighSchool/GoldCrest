c001130d3d0439110f4713d0e60d68d1b1d92de5
Christopher Nethercott <ccnethercott@gmail.com>
2018-12-21 13:20:07 +0000

    Added some Fridges

--- ApplianceControl/ApplianceControl.ino
+++ ApplianceControl/ApplianceControl.ino
@@ -8,6 +8,9 @@
 // Constants
 LiquidCrystal_I2C lcd(0x27, 16, 4); // initalise a 16 by 4 LCD
 
+const int bFridge = 2;
+const int gFridge = 3;
+
 void setup() {
   // put your setup code here, to run once:
 
