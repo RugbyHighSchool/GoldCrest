5f9fbdd2c102b85e2e876b49ff94c36e8f61bae0
Christopher Nethercott <ccnethercott@gmail.com>
2018-12-30 22:20:03 +0000

    https://giphy.com/gifs/shocked-back-to-the-future-christopher-lloyd-13WVgB4UR7hK

--- Python/main.py
+++ Python/main.py
@@ -11,3 +11,28 @@ pins = {
   256: 13,
   512: 14
 }
+
+def subsetsum(array,num):
+    if num == 0 or num < 1:
+        return None
+    elif len(array) == 0:
+        return None
+    else:
+        if array[0] == num:
+            return [array[0]]
+        else:
+            with_v = subsetsum(array[1:],(num - array[0]))
+            if with_v:
+                return [array[0]] + with_v
+            else:
+                return subsetsum(array[1:],num)
+
+def main():
+    ref = subsetsum(numbers, 125)
+    if ref is not None:
+        for pin in ref:
+            print("Turning On Pin {}".format(pins[pin]))
+
+
+if __name__ == '__main__':
+    main()
