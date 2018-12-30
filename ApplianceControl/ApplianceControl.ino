92473eed1e2d8e12ae58df9d1973f75f93fa35b4
Christopher Nethercott <ccnethercott@gmail.com>
2018-12-30 11:48:00 +0000

    Added pins for the eventual deployment

--- ApplianceControl/ApplianceControl.ino
+++ ApplianceControl/ApplianceControl.ino
@@ -8,8 +8,21 @@
 // Constants
 LiquidCrystal_I2C lcd(0x27, 16, 4); // initalise a 16 by 4 LCD
 
-const int bFridge = 2;
-const int gFridge = 3;
+// Appliances
+// g = Good; b = Bad (Inefficient side of the house)
+// f = fridge; tv = tv; dw = dishwasher; wm = washing machine; wh = water heater; 
+const int bF = 2;
+const int bTV = 3;
+const int bDW = 4;
+const int bWM = 5;
+const int bWH = 6;
+const int gF = 12;
+const int gTV = 13;
+const int gDW = 14;
+const int gWM = 15;
+const int gWH = 16;
+
+const int * appliances[] = {&bF, &bTV, &bDW, &bWM, &bWH, &gF, &gTV, &gDW, &gWM, &gWH};
 
 void setup() {
   lcd.begin();
