9e3abbf66d8e55c45086d041829fdbcf6b91a6d6
Christopher Nethercott <ccnethercott@gmail.com>
2018-12-30 12:28:23 +0000

    Now thats a lot of data

--- ApplianceControl/ApplianceControl.ino
+++ ApplianceControl/ApplianceControl.ino
@@ -10,7 +10,7 @@ LiquidCrystal_I2C lcd(0x27, 16, 4); // initalise a 16 by 4 LCD
 
 // Appliances
 // g = Good; b = Bad (Inefficient side of the house)
-// f = fridge; tv = tv; dw = dishwasher; wm = washing machine; wh = water heater; 
+// f = fridge; tv = tv; dw = dishwasher; wm = washing machine; wh = water heater;
 const int bF = 2;
 const int bTV = 3;
 const int bDW = 4;
@@ -24,10 +24,62 @@ const int gWH = 16;
 
 const int * appliances[] = {&bF, &bTV, &bDW, &bWM, &bWH, &gF, &gTV, &gDW, &gWM, &gWH};
 
+// Electricity Price from the 07062017 in half hour intervals.
+const int[] electrictyPrice = {2,
+1.5,
+-1.11,
+-1.11,
+-1.43,
+-1.43,
+1.69,
+2.53,
+5,
+9.5,
+25.14,
+27,
+35,
+38.52,
+44,
+44.49,
+39,
+38.28,
+34.11,
+34,
+34.5,
+34.5,
+36,
+35.82,
+35,
+35,
+35.12,
+34.42,
+29.99,
+29.8,
+36,
+40.62,
+39.64,
+39.89,
+48,
+48,
+50.8,
+50.65,
+52.5,
+52.65,
+56.5,
+55,
+50,
+47,
+46,
+44.5,
+34.79,
+32.79,
+}
+const
+
 void setup() {
   lcd.begin();
 
-  for(int i = 0; i < sizeof(appliances); i++) { 
+  for(int i = 0; i < sizeof(appliances); i++) {
     pinMode(appliances[i], OUTPUT); // turns the pin to output.
     digitalWrite(appliances[i], LOW); // turns the led off
   }
