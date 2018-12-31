399726613dceea21b03c68d7a33452c8444c0cd6
Christopher Nethercott <ccnethercott@gmail.com>
2018-12-31 15:00:57 +0000

    Oh I thought this was commited

---
+++ Vulcan/main.py
@@ -0,0 +1,39 @@
+numbers = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512]
+pins = {
+  # good appliances
+  1: 2,
+  2: 3,
+  4: 3,
+  8: 4,
+  16: 5,
+  # bad appliances
+  32: 10,
+  64: 11,
+  128: 12,
+  256: 13,
+  512: 14
+}
+
+def subsetsum(array,num):
+    if num == 0 or num < 1:
+        return None
+    elif len(array) == 0:
+        return None
+    else:
+        if array[0] == num:
+            return [array[0]]
+        else:
+            with_v = subsetsum(array[1:], (num - array[0]))
+            if with_v:
+                return [array[0]] + with_v
+            else:
+                return subsetsum(array[1:],num)
+
+def main():
+    compositeNumbers = subsetsum(numbers, 429)
+    if compositeNumbers is not None:
+        for pin in compositeNumbers:
+            print("Turning On Pin {}".format(pins[pin]))
+
+if __name__ == '__main__':
+    main()
