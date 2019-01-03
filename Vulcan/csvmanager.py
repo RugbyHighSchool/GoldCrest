b1b43ba3b7318991a0a2dfcf512aa1af3b93b1fa
Christopher Nethercott <ccnethercott@gmail.com>
2019-01-03 20:40:24 +0000

    Removed old variables

--- Vulcan/csvmanager.py
+++ Vulcan/csvmanager.py
@@ -4,11 +4,9 @@ from datetime import datetime
 
 class CSVParser:
     def __init__(self, filepath):
-        self.filepath = filepath
-        with open(self.filepath, newline='') as file:
-            self.file = file
+        with open(filepath, newline='') as file:
             self.dictionary = {}
-            self.csvfile = csv.reader(self.file)
+            self.csvfile = csv.reader(file)
             for row in self.csvfile:
                 self.dictionary[parser.parse(row[0])] = float(row[1])
 
