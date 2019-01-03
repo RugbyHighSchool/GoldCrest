bae3c1ef9c258bd556564cf6ed07b49080bf2f32
Christopher Nethercott <ccnethercott@gmail.com>
2019-01-03 17:10:48 +0000

    Renamed CSVManager to CSVParser

--- Vulcan/csvmanager.py
+++ Vulcan/csvmanager.py
@@ -1,7 +1,7 @@
 import csv
 from datetime import time
 
-class CSVManager:
+class CSVParser:
     def __init__(self, filepath):
         self.filepath = filepath
         with open(self.filepath, newline='') as file:
@@ -12,7 +12,7 @@ class CSVManager:
                 dataTime = row[0].split(":")
                 self.dictionary[time(int(dataTime[0]), int(dataTime[1]), 0)] = float(row[1])
 
-x = CSVManager('electricityprice.csv')
+x = CSVParser('electricityprice.csv')
 
 for k, v in x.dictionary.items():
     print('{:%H:%M} with Price £{} per MWHr'.format(k, v))
