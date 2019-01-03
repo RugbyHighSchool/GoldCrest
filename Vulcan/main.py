b1479dbe5fe3db308847aa60d4d4e594653259b2
Christopher Nethercott <ccnethercott@gmail.com>
2019-01-03 15:01:39 +0000

    Rewrote the main loop to be more friendly

--- Vulcan/main.py
+++ Vulcan/main.py
@@ -9,7 +9,7 @@ appliances = {
   32: [10, "Inefficient Fridge"],
   64: [11, "Inefficient TV"],
   128: [12, "Inefficient Dishwasher"],
-  256: [13, "Inefficient Washing Machine",
+  256: [13, "Inefficient Washing Machine"],
   512: [14, "Inefficient Water Heater"]
 }
 
@@ -42,12 +42,10 @@ def subsetsum(array,num):
                 return subsetsum(array[1:],num)
 
 def main():
-    for time, price in electricityPrice.items():
-        print("Time {} with Price {}".format(time, price))
-    compositeNumbers = subsetsum(list(pins.keys()), 429)
-    if compositeNumbers is not None:
-        for pin in compositeNumbers:
-            print("Turning On Pin {} which corresponds to {}".format(pins[pin], appliances[pins[pin]]))
+    totalUsage = input("What is your usage sum address? ")
+    addresses = subsetsum(list(appliances.keys()), int(totalUsage))
+    for pin in addresses:
+        print("Turning on Pin {} with Description {} with address {}".format(appliances[pin][0], appliances[pin][1], pin))
 
 if __name__ == '__main__':
     main()
