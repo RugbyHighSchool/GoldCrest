8cb371846ed99f95be1fcf4f76fdbb08afab54b8
Christopher Nethercott <ccnethercott@gmail.com>
2019-01-03 17:09:45 +0000

    Inital CSVManager

---
+++ Vulcan/csvmanager.py
@@ -0,0 +1,18 @@
+import csv
+from datetime import time
+
+class CSVManager:
+    def __init__(self, filepath):
+        self.filepath = filepath
+        with open(self.filepath, newline='') as file:
+            self.file = file
+            self.dictionary = {}
+            self.csvfile = csv.reader(self.file)
+            for row in self.csvfile:
+                dataTime = row[0].split(":")
+                self.dictionary[time(int(dataTime[0]), int(dataTime[1]), 0)] = float(row[1])
+
+x = CSVManager('electricityprice.csv')
+
+for k, v in x.dictionary.items():
+    print('{:%H:%M} with Price £{} per MWHr'.format(k, v))
