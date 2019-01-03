6acd857d48ac5ceddb1a65784e76130a67068f80
Christopher Nethercott <ccnethercott@gmail.com>
2019-01-03 17:15:55 +0000

    Stuff & testing

--- Vulcan/csvmanager.py
+++ Vulcan/csvmanager.py
@@ -9,10 +9,11 @@ class CSVParser:
             self.dictionary = {}
             self.csvfile = csv.reader(self.file)
             for row in self.csvfile:
+                print(length(row[0]))
                 dataTime = row[0].split(":")
                 self.dictionary[time(int(dataTime[0]), int(dataTime[1]), 0)] = float(row[1])
 
-x = CSVParser('electricityprice.csv')
+x = CSVParser('dateelectricityprice.csv')
 
 for k, v in x.dictionary.items():
-    print('{:%H:%M} with Price £{} per MWHr'.format(k, v))
+#    print('{:%H:%M} with Price £{} per MWHr'.format(k, v))
