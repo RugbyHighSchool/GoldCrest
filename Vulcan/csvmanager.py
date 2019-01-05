80f8bd1c4fea1e4ac4d1b2a08987f6104b65451d
Christopher Nethercott <ccnethercott@gmail.com>
2019-01-05 16:46:08 +0000

    Renamed classes

--- Vulcan/csvmanager.py
+++ Vulcan/csvmanager.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 dateTimeParse = '%d/%m/%Y %H:%M:%S'
 
-class CSVParser:
+class DatePrice:
     def __init__(self, filepath):
         with open(filepath, newline='') as file:
             self.dictionary = {}
@@ -12,13 +12,23 @@ class CSVParser:
                 if len(row) == 2: # Date and Price
                     self.dictionary[datetime.strptime(row[0], dateTimeParse)] = float(row[1])
 
+    def printDictionary(self):
+        for k, v in self.dictionary.items():
+            print("{} : {} (£/MWhr) : {} (Pence/KWhr)".format(k, v, v / 10))
+
+    def getLowestPrice(self):
+        lowest = min(self.dictionary, key = self.dictionary.get)
+        return { lowest : self.dictionary[lowest] }
+
+class DatePriceUsage:
+    def __init__(self, filepath):
+        with open(filepath, newline='') as file:
+            self.dictionary = {}
+            self.csvfile = csv.reader(file)
+            for row in self.csvfile:
                 if len(row) == 3: # Date, Price and Usage
                     self.dictionary[datetime.strptime(row[0], dateTimeParse)] = [float(row[1]), int(row[2])]
 
     def printDictionary(self):
         for k, v in self.dictionary.items():
-            print("{} : {} (£/MWhr) : {} (Pence/KWhr)".format(k, v[0], v[0] / 10))
-
-    def getLowestPrice(self):
-        lowest = min(self.dictionary, key = self.dictionary.get)
-        return { lowest : self.dictionary[lowest] }
+            print("{} : {} (£/MWhr) : {} (Pence/KWhr) : {} Usage Address".format(k, v[0], v[0] / 10, v[1]))
